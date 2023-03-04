52842f05956b40de0d29a39b480c309785747ed8
Talita-S <talitassantos2015@gmail.com>
2023-03-04 12:26:15 -0300

    Configurando as rotas

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,8 @@
 import {Router} from 'express'; 
 // Importa TestController
 import TesteController from "./controllers/TesteController"
+// Importa ProdutoController
+import ProdutoController from "./controllers/ProdutoController"
 // Validação dos parâmetos da rota
 import ValidaTeste1 from './middlewares/ValidaTeste1';
 // Instancia roteador 
@@ -15,4 +17,11 @@ Roteador.get(
     ValidaTeste1,
     new TesteController().teste1
 );
+
+Roteador.get('/produtos', new ProdutoController().index);
+Roteador.get('/produtos/:id', new ProdutoController().show);
+Roteador.post('/produtos', new ProdutoController().store);
+Roteador.put('/produtos/:id', new ProdutoController().update);
+Roteador.delete('/produtos/:id', new ProdutoController().delete);
+
 export default Roteador;
